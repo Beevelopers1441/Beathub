1dff298aeed84f94620617549d5392f5abd38061
SangjinH <gkstkdwls1994@gmail.com>
2021-09-14 15:14:56 +0900

    feat: 초기설정

---
+++ BE/beathub/src/main/java/com/beeveloper/beathub/BeathubApplication.java
@@ -0,0 +1,12 @@
+package com.beeveloper.beathub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeathubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeathubApplication.class, args);
+	}
+}
---
+++ BE/beathub/src/test/java/com/beeveloper/beathub/BeathubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beeveloper.beathub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeathubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

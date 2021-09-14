baffae62f02132e5b03c81ec5ed6050eb429e41d
SangjinH <gkstkdwls1994@gmail.com>
2021-09-14 15:14:02 +0900

    feat: Post 엔티티 추가

---
+++ BE/beathub/src/main/java/com/beeveloper/beathub/post/domain/Post.java
@@ -0,0 +1,43 @@
+package com.beeveloper.beathub.post.domain;
+
+import com.beeveloper.beathub.group.domain.Group;
+import com.beeveloper.beathub.instrument.domain.Instrument;
+import com.beeveloper.beathub.user.domain.User;
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private boolean isRecruiting = true;
+
+    private LocalDateTime createTime;
+
+    // 아래 User와 Group 둘 중 하나는 빈 값이 될 수 있음
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User authorUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Group authorGroup;
+
+
+    @ManyToMany
+    private List<User> likeUsers = new ArrayList<User>();
+
+    @ManyToOne
+    private Instrument tag;
+
+
+}

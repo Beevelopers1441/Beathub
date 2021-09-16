cfa08f23b5bb41f12432d84b2920cc300d0fe6f5
류영석 <rhyuys115@naver.com>
2021-09-17 00:54:44 +0900

    feat: 개인 모집글 생성 Swagger 설정 추가

--- BE/beathub/src/main/java/com/beeveloper/beathub/post/controller/PostController.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/post/controller/PostController.java
@@ -2,6 +2,9 @@ package com.beeveloper.beathub.post.controller;
 
 import com.beeveloper.beathub.post.dto.PostCreateRequestDto;
 import com.beeveloper.beathub.post.service.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "게시판 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/posts")
@@ -17,7 +21,9 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/member")
-    public ResponseEntity createMemberPost(@RequestBody PostCreateRequestDto createInfo) {
+    @ApiOperation(value = "개인 구인글 생성", notes = "인증한 사용자를 방장으로 하는 스터디 생성")
+    public ResponseEntity createMemberPost(
+            @RequestBody @ApiParam(value = "개인 구인글 생성 정보", required = true) PostCreateRequestDto createInfo) {
         postService.createMemberPost(createInfo);
         return ResponseEntity.ok(null);
     }

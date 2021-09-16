ac6801ee716ec236d34630ead3ce2bf2050edd84
류영석 <rhyuys115@naver.com>
2021-09-17 00:55:07 +0900

    feat: 개인 모집글 생성 Dto Swagger 설정 추가

--- BE/beathub/src/main/java/com/beeveloper/beathub/post/dto/PostCreateRequestDto.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/post/dto/PostCreateRequestDto.java
@@ -1,8 +1,10 @@
 package com.beeveloper.beathub.post.dto;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 
 @Getter
+@ApiModel("생성 정보")
 public class PostCreateRequestDto {
     private String title;
     private String content;

9e65fd9705adffea26e2c3ac6dff4d65d4c5e6bd
SangjinH <gkstkdwls1994@gmail.com>
2021-09-20 22:55:02 +0900

    feat: google OAuth2 로그인을 위한 Entity, 설정 추가

---
+++ BE/auth/src/main/java/com/beeveloper/auth/member/MemberService.java
@@ -0,0 +1,11 @@
+package com.beeveloper.auth.member;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    public void googleJoin(SocialJoinVO sjVO) {
+
+    }
+}
---
+++ BE/auth/src/main/java/com/beeveloper/auth/member/User.java
@@ -0,0 +1,8 @@
+package com.beeveloper.auth.member;
+
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//public class User extends BaseTimeEntity{
+//}

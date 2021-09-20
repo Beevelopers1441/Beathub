4150569fd581561441feeb4c8fa0f50a49944811
sungyujeon <sungyujeon91@gmail.com>
2021-09-20 20:28:22 +0900

    feat: 커뮤니티 글 스타일 적용, detail link 연결
    - 커뮤니티 글 레이아웃 및 디자인 변경
    - 글 리스트에서 detail로 이동하는 routing 설정

--- FE/src/components/Community/Posts/Post/index.tsx
+++ FE/src/components/Community/Posts/Post/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // types
 import { IPost } from 'types';
 
 // styles
+import { Grid } from '@mui/material';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
 import Wrapper from './styles';
 
 interface Props {
@@ -11,13 +14,72 @@ interface Props {
 }
 
 function Post({ post }: Props): React.ReactElement {
+  const history = useHistory();
+
+  // imageUrl tmp
+  const [imageUrl, setImageUrl] = useState('')
+  useEffect(() => {
+    console.log('???')
+    const request = async () => {
+      const res = await fetch(post.userInfo.imageUrl, {
+        method: 'GET'
+      })
+      const data = await res.json()
+      const _imageUrl = data[0].url;
+      setImageUrl(_imageUrl)
+    }
+    request()
+  }, [post])
+
+  const handlePostDetail = (postId: number) => {
+    const location = { pathname: `/post/${postId}`};
+    history.push(location);
+  }
 
   return (
     <Wrapper>
-      <p>제목: {post.title}</p>
-      <p>내용: {post.content}</p>
-      <p>tags: {post.tags}</p>
-      <p>status: {post.status}</p>
+      <Grid container>
+        <Grid item xs={10}
+          className="mainInfo-box"
+          onClick={() => handlePostDetail(post.id)}
+        >
+          <Grid container>
+            <Grid item xs={9}
+              className="post-mainInfo-container"
+            >
+              <p className="title">{post.title}</p>
+              <p className="content">{post.content}</p>
+              <p className="tags">{post.tags.map(tag => `#${tag} `)}</p>
+            </Grid>
+            <Grid item xs={3}
+              className="post-subInfo-container"
+            >
+              <p className="comment">{post.comments.length}개의 댓글</p>
+              <div className="time-likes-container">
+                <p className="time">{post.created_at}</p>
+                <div className="likes-container">
+                  <Favorite className="likes-icon"/>
+                  <p>{post.likes}</p>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2}
+          className="user-container"
+        >
+          <p className="user-name">{post.userInfo.name}</p>
+          <img 
+            src={imageUrl}
+            alt="img"
+            className="user-image"
+          />
+        </Grid>
+      </Grid>
+      
+      {/* <p>status: {post.status}</p>
+      <p>recruitStatus: {post.recruitStatus}</p> */}
+
 
     </Wrapper>
   )
--- FE/src/components/Community/Posts/Post/styles.ts
+++ FE/src/components/Community/Posts/Post/styles.ts
@@ -1,7 +1,84 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  
+  width: 100%;
+  border-top: 0.5px solid #ABB0B5;
+  padding: 1.5rem;
+
+  & .mainInfo-box {
+    cursor: pointer;
+  }
+
+  & .post-mainInfo-container {
+    & .title {
+      font-size: 1.4rem;
+      margin-bottom: 1rem;
+    }
+    & .content {
+      color: ${({ theme }) => theme.colors.subGrey };
+      font-size: 1rem;
+      font-weight: 300;
+      margin-bottom: 1rem;
+    }
+    & .tags {
+      color: ${({ theme }) => theme.colors.subGrey };
+      font-size: 1rem;
+      font-weight: 300;
+    }
+  }
+
+  & .post-subInfo-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-weight: 100;
+
+    & .comment {
+      text-align: end;
+      font-size: 0.8rem;
+    }
+    & .time-likes-container {
+      display: flex;
+      justify-content: end;
+
+      & .time {
+        font-size: 0.8rem;
+        align-self: center;
+        margin-right: 0.6rem;
+      }
+      
+      & .likes-container {
+        display: flex;  
+        align-items: center;
+        font-size: 0.8rem;
+        
+        & .likes-icon {
+          font-size: 0.9rem;
+          margin-right: 0.2rem;
+        }
+      }
+    }
+  }
+
+  & .user-container {
+    display: flex;
+    justify-content: end;
+
+    & .user-name {
+      margin-top: 0.2rem;
+      font-size: 13px;
+    }
+    & .user-image {
+      display: inline-block;
+      width: 50px;
+      height: 50px;
+      margin-left: 0.4rem;
+      border-radius: 50%;
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: cover;
+    }
+  }
 `
 
 export default Wrapper

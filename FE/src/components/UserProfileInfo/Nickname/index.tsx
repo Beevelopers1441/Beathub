80db9be2f9f713fe358162ae38136a876bf86c83
tjatn304905 <tjatn304905@gmail.com>
2021-09-23 08:21:08 +0900

    feat: 닉네임, 자기소개 컴포넌트 생성

---
+++ FE/src/components/UserProfileInfo/Nickname/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from './styles';
+
+function Nickname() {
+  return(
+    <Wrapper>
+      <div className="nickname">Sumsoo</div>
+    </Wrapper>
+  )
+}
+
+export default Nickname;
--- FE/src/components/UserProfileInfo/index.tsx
+++ FE/src/components/UserProfileInfo/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ProfileImg from './ProfileImg';
+import Nickname from './Nickname';
+import Intro from './Intro';
 
 function UserProfileInfo() {
   return (
     <div>
       <ProfileImg></ProfileImg>
+      <Nickname></Nickname>
+      <Intro></Intro>
     </div>
   )
 }

679b0d87f6d26274ad934416916a68dfadee3fdc
Helia <jyk.helia@gmail.com>
2021-09-23 16:14:53 +0900

    feat: 소셜로그인 CORS 프록시 설정

--- FE/src/lib/api/auth/socialLogin.ts
+++ FE/src/lib/api/auth/socialLogin.ts
@@ -5,9 +5,11 @@ import { UserInfo } from 'types';
 
 const BASE_URL = 'http://localhost:8100'
 
+
 export const socialLogin = (userInfo: UserInfo) => {
+    
     console.log(userInfo)
-    axios.post(`${BASE_URL}/api/auth/login`, userInfo)
+    axios.post('/api/auth/login', userInfo)
         .then(response => console.log(response));
 }
 

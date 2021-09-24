4ef62544a2e215c33d8ad236b019453451edea31
tjatn304905 <tjatn304905@gmail.com>
2021-09-24 10:58:21 +0900

    feat: 타입스크립트용 리덕스 모듈 설치

--- FE/src/modules/index.ts
+++ FE/src/modules/index.ts
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
+import auth from './auth';
 
 export type RootState = {
 
 };
 
 const rootReducer = combineReducers({
-  
+  auth,
 });
 
 export default rootReducer;

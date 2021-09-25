f6c543f21adff67a3b4f771854206ace4a8fb7e0
tjatn304905 <tjatn304905@gmail.com>
2021-09-26 00:35:05 +0900

    style: 프로필 네비게이션 바 스타일 지정

--- FE/src/components/UserProfileBoard/Navbar/styles.tsx
+++ FE/src/components/UserProfileBoard/Navbar/styles.tsx
@@ -1,7 +1,33 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  
+  & .tab-container {
+    margin-left: 1rem;
+    display: flex;
+  }
+
+  & .tab-title {
+    margin: 0 1rem 0.7rem 1rem;
+    font-family: Spoqa Han Sans Neo;
+    font-style: normal;
+    line-height: 33px;
+    text-align: center;
+    color: #ABB0B5;
+  }
+
+  & .tab-title-active {
+    margin: 0 1rem 0.7rem 1rem;
+    font-family: Spoqa Han Sans Neo;
+    font-style: normal;
+    line-height: 33px;
+    text-align: center;
+    color: #FFFFFF;
+  }
+
+  & .nav-divider {
+    margin-left: 1rem;
+    border-bottom: 0.5px solid white;
+  }
 `
 
 export default Wrapper

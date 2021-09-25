78f9f936ab165183a3705428f7afac970ff68561
tjatn304905 <tjatn304905@gmail.com>
2021-09-25 10:24:53 +0900

    resolve: fix conflict

--- FE/src/pages/UserProfile/index.tsx
+++ FE/src/pages/UserProfile/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Components
 import { Header } from 'components';
-import UserProfileInfo from '../../components/UserProfileInfo';
+import UserProfileInfo from 'components/UserProfileInfo';
 
 // styles
 import { Container, Grid } from '@mui/material';

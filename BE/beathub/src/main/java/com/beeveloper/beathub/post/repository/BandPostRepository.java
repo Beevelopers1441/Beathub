fb3487fb3286ffc880012a39fd6bd55b08914655
SangjinH <gkstkdwls1994@gmail.com>
2021-09-27 21:21:54 +0900

    feat: BandPost 설정

--- BE/beathub/src/main/java/com/beeveloper/beathub/post/repository/BandPostRepository.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/post/repository/BandPostRepository.java
@@ -4,6 +4,10 @@ import com.beeveloper.beathub.post.domain.BandPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BandPostRepository extends JpaRepository<BandPost, Long> {
+    @Override
+    Optional<BandPost> findById(Long aLong);
 }

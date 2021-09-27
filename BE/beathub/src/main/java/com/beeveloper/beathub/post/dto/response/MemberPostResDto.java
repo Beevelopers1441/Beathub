5f5dce2868d0fa3e95b8abb77513ea2fc566431e
SangjinH <gkstkdwls1994@gmail.com>
2021-09-27 21:23:15 +0900

    feat: MemberPost 설정

--- BE/beathub/src/main/java/com/beeveloper/beathub/post/dto/response/MemberPostResDto.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/post/dto/response/MemberPostResDto.java
@@ -1,13 +1,19 @@
 package com.beeveloper.beathub.post.dto.response;
 
+import com.beeveloper.beathub.common.dto.TagInfoDto;
+import com.beeveloper.beathub.common.dto.UserInfoDto;
 import com.beeveloper.beathub.post.domain.BandPost;
+import com.beeveloper.beathub.post.domain.Comment;
 import com.beeveloper.beathub.post.domain.MemberPost;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Getter
@@ -23,11 +29,20 @@ public class MemberPostResDto {
     @ApiModelProperty(name="구인글 내용", example = "내용입니다.")
     private String content;
 
+    @ApiModelProperty(name = "작성자", example = "한상진")
+    private UserInfoDto author;
+
     @ApiModelProperty(name="모집 여부", example = "true")
     private boolean isRecruiting;
 
     @ApiModelProperty(name="작성 시간", example = "2021-09-15")
-    private LocalDateTime createTime;
+    private String createTime;
+
+    private List<CommentResDto> comments;
+
+    private List<UserInfoDto> likeUsers;
+
+    private TagInfoDto tag;
 
     public static MemberPostResDto of(MemberPost memberPost) {
         MemberPostResDto memberPostResDto = new MemberPostResDto();
@@ -35,7 +50,11 @@ public class MemberPostResDto {
         memberPostResDto.title = memberPost.getTitle();
         memberPostResDto.content = memberPost.getContent();
         memberPostResDto.isRecruiting = memberPost.isRecruiting();
-        memberPostResDto.createTime = memberPost.getCreateTime();
+        memberPostResDto.createTime = LocalDateTime.now().toString();
+        memberPostResDto.likeUsers = UserInfoDto.ofUser(memberPost.getLikeUsers());
+        memberPostResDto.author = UserInfoDto.ofUser(memberPost.getAuthorUser());
+        memberPostResDto.comments = CommentResDto.of(memberPost.getComments());
+        memberPostResDto.tag = TagInfoDto.of(memberPost.getTag());
         return memberPostResDto;
     }
 

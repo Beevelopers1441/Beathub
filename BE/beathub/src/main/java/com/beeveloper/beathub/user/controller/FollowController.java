72e3a6657b02f8598dbe83f4e847622f19df4ced
SangjinH <gkstkdwls1994@gmail.com>
2021-09-29 00:37:40 +0900

    feat: user간 팔로우구현

---
+++ BE/beathub/src/main/java/com/beeveloper/beathub/user/controller/FollowController.java
@@ -0,0 +1,62 @@
+package com.beeveloper.beathub.user.controller;
+
+import com.beeveloper.beathub.common.dto.FollowDto;
+import com.beeveloper.beathub.common.dto.UserInfoDto;
+import com.beeveloper.beathub.user.domain.Follow;
+import com.beeveloper.beathub.user.domain.User;
+import com.beeveloper.beathub.user.jwts.JwtService;
+import com.beeveloper.beathub.user.repository.FollowRepository;
+import com.beeveloper.beathub.user.service.FollowService;
+import com.beeveloper.beathub.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class FollowController {
+
+    private final FollowRepository followRepository;
+    private final FollowService followService;
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @PostMapping("/follow/{toUserId}")
+    public void followUser(
+            @RequestHeader(value = "Authorization") String jwtToken,
+            @PathVariable Long toUserId) {
+        followService.save(userService.findByEmail(jwtService.getProperties(jwtToken).get("email")).getName(), toUserId);
+    }
+
+    @DeleteMapping("/follow/{toUserId}")
+    public void unFollowUser(
+            @RequestHeader(value = "Authorization") String jwtToken,
+            @PathVariable Long toUserId
+    ) {
+        Long id = followService.getFollowIdByFromEmailToId(userService.findByEmail(jwtService.getProperties(jwtToken).get("email")).getEmail(), toUserId);
+        followRepository.deleteById(id);
+    }
+
+
+    @GetMapping("/follow/{userId}")
+    public Map<String, List<FollowDto>> getInfos(
+            @PathVariable Long userId
+    ) {
+        List<Follow> followers = followService.getFollowers(userId);
+        List<Follow> followings = followService.getFollowings(userId);
+
+
+        List<FollowDto> followersDto = FollowDto.ofFollower(followers);
+        List<FollowDto> followingDto = FollowDto.ofFollowing(followings);
+        Map<String, List<FollowDto>> result = new HashMap<>();
+        result.put("followers", followersDto);
+        result.put("followings", followingDto);
+
+        return result;
+    }
+}

b6080ff86338921a09ba582b50c82caa6fbbe72d
SangjinH <gkstkdwls1994@gmail.com>
2021-09-29 16:37:59 +0900

    feat: UserInstrument 여러개 설정 가능하게끔 변경

--- BE/beathub/src/main/java/com/beeveloper/beathub/user/service/UserInstrumentService.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/user/service/UserInstrumentService.java
@@ -4,6 +4,7 @@ import com.beeveloper.beathub.instrument.domain.Instrument;
 import com.beeveloper.beathub.user.domain.User;
 import com.beeveloper.beathub.user.domain.UserInstrument;
 import com.beeveloper.beathub.user.domain.dto.request.UserInstrumentCreateDto;
+import com.beeveloper.beathub.user.domain.dto.response.UserInstrumentResDto;
 
 import java.util.List;
 
@@ -14,4 +15,10 @@ public interface UserInstrumentService {
     UserInstrument save(UserInstrumentCreateDto dto);
 
     List<UserInstrument> findAllByUser(User user);
+
+    UserInstrument findByUserAndInstrument(User user, Instrument instrument);
+
+    UserInstrument update(UserInstrument findUserInstrument, UserInstrumentResDto dto);
+
+    void delete(UserInstrument userInstrument);
 }

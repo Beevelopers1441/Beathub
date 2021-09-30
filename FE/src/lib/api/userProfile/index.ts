5a321f4bf1421394f6b69ed25700dcc8e0278a15
tjatn304905 <tjatn304905@gmail.com>
2021-09-30 14:51:10 +0900

    feat: 개인 팔로우/언팔로우 api 작성

--- FE/src/lib/api/userProfile/index.ts
+++ FE/src/lib/api/userProfile/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import { config } from 'process';
 
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
+const TOKEN = localStorage.getItem('token');
 
 const getUserProfile = async (userId: Number) => {
   const url = `${BASE_URL}/api/user/{userId}`;
@@ -9,9 +11,33 @@ const getUserProfile = async (userId: Number) => {
       userId: userId
     }
   });
-
   return response
 };
 
+// 개인 팔로우
+const followUser = async (toUserId: Number) => {
+  const config: any = {
+    method: 'POST',
+    url:`${BASE_URL}/api/follow/${toUserId}`,
+    headers: {
+      Authorization: TOKEN,
+    }
+  }
+  return await axios(config);
+};
 
-export { getUserProfile };
+// 개인 언팔로우
+const unFollowUser = async (toUserId: Number) => {
+  const config: any = {
+    method: 'DELETE',
+    url:`${BASE_URL}/api/follow/${toUserId}`,
+    headers: {
+      Authorization: TOKEN,
+    }
+  }
+  return await axios(config);
+};
+
+
+
+export { getUserProfile, followUser };

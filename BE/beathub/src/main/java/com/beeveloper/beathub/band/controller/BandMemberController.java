ad6dce772507bfddd23127513e3a793d78765961
SangjinH <gkstkdwls1994@gmail.com>
2021-09-30 19:23:25 +0900

    feat: Band가입 요청 승인, 거절 메서드 추가

--- BE/beathub/src/main/java/com/beeveloper/beathub/band/controller/BandMemberController.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/band/controller/BandMemberController.java
@@ -2,6 +2,7 @@ package com.beeveloper.beathub.band.controller;
 
 import com.beeveloper.beathub.band.domain.Band;
 import com.beeveloper.beathub.band.domain.BandMember;
+import com.beeveloper.beathub.band.dto.request.BandMemberRegisterDto;
 import com.beeveloper.beathub.band.dto.ressponse.BandMemberResDto;
 import com.beeveloper.beathub.band.service.BandMemberService;
 import com.beeveloper.beathub.band.service.BandService;
@@ -9,14 +10,14 @@ import com.beeveloper.beathub.user.domain.User;
 import com.beeveloper.beathub.user.jwts.JwtService;
 import com.beeveloper.beathub.user.service.UserService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.Response;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/bandMember")
@@ -29,43 +30,98 @@ public class BandMemberController {
     private final JwtService jwtService;
     private final UserService userService;
 
-    @Transactional
-    @ApiOperation(value = "밴드이름으로 밴드멤버를 조회하는 API입니다")
-    @GetMapping("/{bandName}")
-    public ResponseEntity<List<BandMemberResDto>> members(
-            @RequestParam(value = "bandName") String bandName
-    ) {
-        List<BandMemberResDto> members = bandMemberService.findMembers(bandName);
-
-        return ResponseEntity.status(201).body(members);
-    }
 
     @ApiOperation(value = "밴드에 가입신청하는 API입니다.")
     @PostMapping("/{bandId}")
-    public ResponseEntity<BandMemberResDto> apply(
+    public ResponseEntity apply(
             @RequestHeader(value = "Authorization") String jwtToken,
             @PathVariable(value = "bandId") Long bandId) {
 
-        User findUser = userService.findByEmail(jwtService.getProperties(jwtToken).get("email"));
+        Optional<User> searchUser = jwtService.returnUser(jwtToken);
+        if (!searchUser.isPresent()) {
+            return ResponseEntity.badRequest().body("회원가입을 해주세요!");
+        }
+        User findUser = searchUser.get();
 
         if (findUser == null) {
-            return (ResponseEntity) ResponseEntity.badRequest();
+            return ResponseEntity.status(403).build();
         } else {
             if (bandMemberService.findMemberInBand(bandId, findUser) != null) {
-                return (ResponseEntity<BandMemberResDto>) ResponseEntity.badRequest();}
-
+                return ResponseEntity.badRequest().body("이미 처리된 요청입니다!");
+            }
         }
 
         BandMember apply = bandMemberService.apply(bandId, findUser);
         return ResponseEntity.status(200).body(BandMemberResDto.of(apply));
     }
 
+
     @ApiOperation(value = "밴드에 가입신청한 User를 반환해주는 API")
     @GetMapping("/waiting/{bandId}")
-    public ResponseEntity<List<BandMemberResDto>> getWaitingMembers(
+    public ResponseEntity getWaitingMembers(
             @PathVariable(value = "bandId") Long bandId) {
 
+        Optional<Band> searchBand = bandService.findById(bandId);
+
+        if (!searchBand.isPresent()) {
+            return ResponseEntity.badRequest().body("존재하지 않는 밴드입니다.");
+        }
+
         List<BandMember> watingMember = bandMemberService.findWatingMember(bandId);
+
         return ResponseEntity.status(200).body(BandMemberResDto.of(watingMember));
+    }
+
+
+    @Transactional
+    @ApiOperation(value = "밴드 가입신청을 승인하는 API")
+    @PostMapping("/approve")
+    public void approveBandMember(
+            @RequestHeader(value = "Authorization") String jwtToken,
+            @RequestBody @ApiParam BandMemberRegisterDto dto) {
+
+        // 예외 처리, 존재 유무 확인 후, 리더의 Id와 요청한 사람의 Id 가 동일한지 확인
+        Optional<User> leader = jwtService.returnUser(jwtToken);
+
+        Optional<User> requestUser = userService.findById(dto.getUserId());
+        Optional<Band> searchBand = bandService.findById(dto.getBandId());
+        if (!leader.isPresent() || !requestUser.isPresent() || !searchBand.isPresent()) {
+            return;
+        }
+        User leaderUser = leader.get();
+        User user = requestUser.get();
+        Band band = searchBand.get();
+
+        if (leaderUser.getId() != band.getLeader().getId()) {
+            return;
+        }
+
+        bandMemberService.approve(band, user);
+    }
+
+    @Transactional
+    @ApiOperation(value = "밴드 가입신청을 거절하는 API")
+    @PostMapping("/deny")
+    public void denyBandMember(
+            @RequestHeader(value = "Authorization") String jwtToken,
+            @RequestBody @ApiParam BandMemberRegisterDto dto) {
+
+        // 예외 처리, 존재 유무 확인 후, 리더의 Id와 요청한 사람의 Id 가 동일한지 확인
+        Optional<User> leader = jwtService.returnUser(jwtToken);
+
+        Optional<User> requestUser = userService.findById(dto.getUserId());
+        Optional<Band> searchBand = bandService.findById(dto.getBandId());
+        if (!leader.isPresent() || !requestUser.isPresent() || !searchBand.isPresent()) {
+            return;
+        }
+        User leaderUser = leader.get();
+        User user = requestUser.get();
+        Band band = searchBand.get();
+
+        if (!leaderUser.getId().equals(band.getLeader().getId())) {
+            return;
+        }
+
+        bandMemberService.deny(band, user);
     }
 }

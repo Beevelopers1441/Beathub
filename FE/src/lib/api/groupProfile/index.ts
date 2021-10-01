df46c9bd4f920704770aa4c852fb21947e9a9cd0
sungyu_jeon <sungyujeon91@gmail.com>
2021-10-01 17:58:41 +0900

    fix: env 설정 및 url 규칙 변경

--- FE/src/lib/api/groupProfile/index.ts
+++ FE/src/lib/api/groupProfile/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 const getGroupProfile = async (bandId: Number) => {
-  const url = `${BASE_URL}/api/bands/{bandId}`;
+  const url = `${BASE_URL}api/bands/{bandId}`;
   const response = await axios.get(url, {
     params: {
       bandId: bandId

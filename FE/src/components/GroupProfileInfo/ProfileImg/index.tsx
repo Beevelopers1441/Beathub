1d64ced73b98e952f2f47385e3c35bcede271908
tjatn304905 <tjatn304905@gmail.com>
2021-10-01 11:50:02 +0900

    feat: 그룹, 개인 기본  프로필 이미지 생성

--- FE/src/components/GroupProfileInfo/ProfileImg/index.tsx
+++ FE/src/components/GroupProfileInfo/ProfileImg/index.tsx
@@ -5,19 +5,31 @@ import React from 'react';
 
 // styles
 import Wrapper from './styles';
+import defaultProfileImg from 'assets/constants/defaultProfileImg.png';
 
 interface IProps {
   imageUrl: string;
 }
 
 const ProfileImg: React.FC<IProps> = ({ imageUrl }) => {
+
+  // 기본 프로필 이미지 핸들러
+  const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const target = e.target as HTMLImageElement
+    target.src = defaultProfileImg;
+  }
+
   return(
     <Wrapper>
       {/* <div>
         <ProfileImage url={"https://cdnweb01.wikitree.co.kr/webdata/editor/202004/07/img_20200407162305_1f42c686.webp"} />
       </div> */}
-
-      <img className="profile-img" src={imageUrl} alt="" />
+      <img 
+        className="profile-img" 
+        src={imageUrl} 
+        alt="프로필 이미지"
+        onError={(e) => handleImgError(e)}
+      />
     </Wrapper>
   )
 }

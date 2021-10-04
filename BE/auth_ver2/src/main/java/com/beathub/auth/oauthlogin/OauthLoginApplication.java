501a2b84ddb2d87c8c8aa86ccc732deca7aa126b
rhyuys115 <ryeongseok1125@gmail.com>
2021-10-05 06:45:18 +0900

    build: 자동 배포를 위한 main 변경

--- BE/auth_ver2/src/main/java/com/beathub/auth/oauthlogin/OauthLoginApplication.java
+++ BE/auth_ver2/src/main/java/com/beathub/auth/oauthlogin/OauthLoginApplication.java
@@ -2,13 +2,16 @@ package com.beathub.auth.oauthlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class OauthLoginApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OauthLoginApplication.class, args);
+        SpringApplication app = new SpringApplication(OauthLoginApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 
 }
--- BE/beathub/src/main/java/com/beeveloper/beathub/BeathubApplication.java
+++ BE/beathub/src/main/java/com/beeveloper/beathub/BeathubApplication.java
@@ -2,11 +2,14 @@ package com.beeveloper.beathub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BeathubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BeathubApplication.class, args);
+		SpringApplication app = new SpringApplication(BeathubApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());
+		app.run(args);
 	}
 }

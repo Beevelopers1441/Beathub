1f97e131ba12703fe0cce5c601a2124677c5a4e0
tjatn304905 <tjatn304905@gmail.com>
2021-10-06 17:14:59 +0900

    chore: 버킷 정보 디자인 수정

--- FE/src/components/Beathub/BeathubInfo/index.tsx
+++ FE/src/components/Beathub/BeathubInfo/index.tsx
@@ -15,12 +15,12 @@ function BeathubInfo( {bucketInfo}: Props) {
   return (
     <Wrapper>
       <Grid container spacing={1} className="container">
-        <Grid item xs={9}>
+        <Grid item xs={10}>
           <div className="title">{bucketInfo.title}</div>
           <div className="introduction">{bucketInfo.introduction}</div>
         </Grid>
-        <Grid item xs={3}>
-          <Button>
+        <Grid item xs={2}>
+          <Button variant="contained" color="secondary" className="button">
             음악 저장
           </Button>
         </Grid>
--- FE/src/components/Beathub/BeathubInfo/styles.tsx
+++ FE/src/components/Beathub/BeathubInfo/styles.tsx
@@ -4,7 +4,7 @@ const Wrapper = styled.div`
   & .container {
 
     & .title {
-      margin-left: 2rem;
+      margin-left: 4rem;
       font-family: Spoqa Han Sans Neo;
       font-style: normal;
       font-weight: bold;
@@ -12,13 +12,17 @@ const Wrapper = styled.div`
     }
 
     & .introduction {
-      margin-left: 2rem;
+      margin-left: 4rem;
       margin-top: 0.5rem;
       font-family: Spoqa Han Sans Neo;
       font-style: normal;
       font-weight: normal;
       font-size: 16px;
       line-height: 29px;
+    }
+
+    & .button {
+      margin-top: 1rem;
     }
   }
 `;

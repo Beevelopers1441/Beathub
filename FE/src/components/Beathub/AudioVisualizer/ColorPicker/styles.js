7a274369bb004ec1cc926fece94d515dde7224c2
Helia <jyk.helia@gmail.com>
2021-10-08 06:55:30 +0900

    feat: 오디오 비쥬얼라이져 파형 변경 기능 추가

---
+++ FE/src/components/Beathub/AudioVisualizer/ColorPicker/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+& .color-picker {
+  bottom: 100px;
+  left: 50px;
+  padding-top: 1rem;
+  position: absolute;
+}
+
+`;
+
+export default Wrapper;
--- FE/src/lib/api/beathub/chageVisual.js
+++ FE/src/lib/api/beathub/chageVisual.js
@@ -1,32 +1,36 @@
-let colorPalette = ["#FFF07C", "#80FF72", "#7EE8FA", "#EEC0C6", "#E58C8A"]
+// let colorPalette = ["#FFF07C", "#80FF72", "#7EE8FA", "#EEC0C6", "#E58C8A"]
+let colorPalette = ['#B01EFF', '#56CCF2', '#FF6780', '#14F1D9', '#7B42F6', '#ffffff', 'rgba(255, 255, 255, 0)']
+let colorPalette2 = ['#ffffff', 'rgba(255, 255, 255, 0)', '#7B42F6']
+let colorPalette3 = ['#ffffff', '#7B42F6']
+let colorPalette4 = ['#B01EFF', '#56CCF2', 'rgba(255, 255, 255, 0)']
 //"big bars",
 export let initState = {
     waves: ["bars", "bars blocks", "cubes", "dualbars", "dualbars blocks", "fireworks",
         "flower", "flower blocks", "orbs", "ring", "rings", "round wave", "shine", "shine rings", "shockwave",
         "star", "static", "stitches", "web", "wave"],
-    activePreviewWave: "shockwave",
+    activePreviewWave: "web",
     previewWaveColorMap: {
-        "bars": [...colorPalette],
-        "bars blocks": [...colorPalette].slice(0, 2),
+        "bars": [...colorPalette].slice(0, 5),
+        "bars blocks": [...colorPalette].slice(5, 7),
         "big bars": [...colorPalette],
         "cubes": [...colorPalette],
-        "dualbars": [...colorPalette],
-        "dualbars blocks": [...colorPalette].slice(0, 2),
-        "fireworks": [...colorPalette].slice(0, 1),
+        "dualbars": [...colorPalette].slice(0, 6),
+        "dualbars blocks": [...colorPalette].slice(5, 7),
+        "fireworks": [...colorPalette].slice(5, 6),
         "flower": [...colorPalette],
-        "flower blocks": [...colorPalette].slice(0, 2),
+        "flower blocks": [...colorPalette].slice(4, 6),
         "orbs": [...colorPalette].slice(0, 2),
-        "ring": [...colorPalette].slice(0, 2),
-        "rings": [...colorPalette],
-        "round wave": [...colorPalette].slice(0, 2),
+        "ring": [...colorPalette].slice(5, 7),
+        "rings": [...colorPalette].slice(0, 6),
+        "round wave": [...colorPalette].slice(5, 7),
         "shine": [...colorPalette].slice(0, 2),
         "shine rings": [...colorPalette],
         "shockwave": [...colorPalette].slice(0, 3),
-        "star": [...colorPalette].slice(0, 3),
-        "static": [...colorPalette].slice(0, 1),
-        "stitches": [...colorPalette].slice(0, 1),
-        "wave": [...colorPalette].slice(0, 2),
-        "web": [...colorPalette].slice(0, 3)
+        "star": [...colorPalette2].slice(0, 3),
+        "static": [...colorPalette].slice(3, 4),
+        "stitches": [...colorPalette].slice(5, 6),
+        "wave": [...colorPalette3].slice(0, 2),
+        "web": [...colorPalette4].slice(0, 3)
     }
 }
 

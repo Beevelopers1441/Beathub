498ec05addda797bb5f4deccf038c52307e5e455
sungyu_jeon <sungyujeon91@gmail.com>
2021-10-08 11:17:36 +0900

    style: 커뮤니티 디테일 페이지 잘림 방지

--- FE/src/pages/Post/PostDetail/styles.ts
+++ FE/src/pages/Post/PostDetail/styles.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components'
 import Background6 from 'assets/svgs/Background6.svg'
 
 const Wrapper = styled.div`
-  padding-top: 10vh;
-  height: 100vh;
+  padding-top: 100px;
   background-position: center center;
   background-image: url(${Background6});
   background-repeat : no-repeat;
